0be20cb9b95610a396360ebe2ea28e8829aa4bfa
Jyotirmaya Das <43753850+Jitu7@users.noreply.github.com>
2021-06-20 22:26:41 +0530

    Added one more product in main

--- design_patterns_e3/src/main/java/main/Main.java
+++ design_patterns_e3/src/main/java/main/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		Product product = Product.builder().id(10).name("Beer").build();
+		Product product2 = Product.builder().id(11).name("Beer2").build();
+		
 
 	}
 
